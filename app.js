666172ad544283c0282f5a90f7c07fa36e5a6473
Igor Novozhilov <maildrakon@gmail.com>
2021-03-12 13:26:41 +0300

    test.ok

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 import Fastify from 'fastify'
 
-const fastify = Fastify({ logger: true })
+const fastify = Fastify({
+   logger: true
+})
 const PORT = process.env.PORT || 3000
 
 
 fastify
-  .get('/', (req, reply) => reply.send({ test: 'ok' }))
-  .listen(PORT, '0.0.0.0', (err, address) => {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    fastify.log.info(`server listening on ${address}`)
-  })
+   .get('/', getHome)
+   .listen(PORT, '0.0.0.0', (err, address) => {
+      if (err) {
+         fastify.log.error(err)
+         process.exit(1)
+      }
+      fastify.log.info(`server listening on ${address}`)
+   })
+
+
+function getHome(req, reply) {
+   // код тут :)
+   reply.send({
+      test: 'test - ok'
+   })
+}
